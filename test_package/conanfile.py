0dd95964456d3f30574d2c14ab4fa38a5f66fdd5
Uilian Ries <uilianries@gmail.com>
2017-03-04 01:19:07 -0300

    Travis CI support
    - Added Travis CI receipt
    - Python PEP8 code style
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,8 +1,12 @@
+"""Receipt validation for libusb-1.0.21
+"""
 from os import getenv
 from conans import ConanFile, CMake
 
 
-class gperftoolsTestConan(ConanFile):
+class TestLibUSBConan(ConanFile):
+    """Build test using target package and execute all tests
+    """
     channel = getenv("CONAN_CHANNEL", "testing")
     username = getenv("CONAN_USERNAME", "uilianries")
     settings = "os", "compiler", "build_type", "arch"
@@ -11,7 +15,8 @@ class gperftoolsTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self.settings)
-        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
+        self.run('cmake "%s" %s' %
+                 (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def imports(self):
