faf64fa99538c9e45c9b2be58b57e2d30ce28230
Uilian Ries <uilianries@gmail.com>
2017-03-06 20:20:51 -0300

    Override Conan password
    - Capture user password from environment
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,9 +1,11 @@
 """This script build Conan.io package to multiple platforms."""
+from os import getenv
 from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
+    builder.password = getenv("CONAN_PASSWORD")
     builder.add_common_builds(pure_c=True)
     filtered_builds = []
     for settings, options in builder.builds:
