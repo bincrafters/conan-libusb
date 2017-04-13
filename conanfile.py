dc4f6edf71849aa1ebf9bde46814d942b63a8e08
Uilian Ries <uilianries@gmail.com>
2017-04-12 21:57:14 -0300

    Shared library option
    - Added shared option
    - Using new Conan script stuffs
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,14 @@
 """Conan receipt package for USB Library 1.0.21
 """
+from tempfile import mkdtemp
 from os import unlink
+from os.path import join
 from conans import ConanFile
 from conans.tools import download
 from conans.tools import unzip
 from conans.tools import check_md5
+from conans.tools import chdir
+from conans.tools import SystemPackageTool
 from conans.tools import environment_append
 from conans import AutoToolsBuildEnvironment
 
@@ -17,62 +21,47 @@ class LibUSBConan(ConanFile):
     release_name = "%s-%s" % (name, version)
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"udev": [True, False]}
-    default_options = "udev=True"
+    options = {"shared": [True, False],"udev": [True, False]}
+    default_options = "shared=True", "udev=True"
     url = "http://github.com/uilianries/conan-libusb"
     author = "Uilian Ries <uilianries@gmail.com>"
-    license = "https://github.com/libusb/libusb/blob/master/COPYING"
-    prefix_install = "%s/_build" % release_name
+    license = "LGPL-2.1"
+    description = "A cross-platform library to access USB devices"
+    build_dir = mkdtemp(suffix=name)
 
     def source(self):
-        package_name = "%s.tar.bz2" % self.release_name
-        url = "https://github.com/libusb/libusb/releases/download/v%s/%s" % (
-            self.version, package_name)
-        download(url, package_name)
-        check_md5(package_name, "1da9ea3c27b3858fa85c5f4466003e44")
-        unzip(package_name)
-        unlink(package_name)
+        tar_name = "%s-%s.tar.gz" % (self.name, self.version)
+
+        download("https://github.com/libusb/libusb/releases/download/v%s/%s.tar.bz2" % (self.version, self.release_name), tar_name)
+        check_md5(tar_name, "1da9ea3c27b3858fa85c5f4466003e44")
+        unzip(tar_name)
+        unlink(tar_name)
 
     def system_requirements(self):
-        self.global_system_requirements=True
-        if self.settings.os == "Linux":
-            self.output.warn("'libudev' library is required in your computer.")
-            # (uilianries): Could not possible install i386 and amd64 versions at same time.
-            arch = "amd64" if self.settings.arch == "x86_64" else "i386"
-            self.run("sudo apt-get update && sudo apt-get install -y libudev-dev:%s || true " % arch)
+        if self.options.udev:
+            package_tool = SystemPackageTool()
+            arch = "i386" if self.settings.arch == "x86" else "amd64"
+            package_tool.install(packages="libudev-dev:%s" % arch, update=True)
 
     def build(self):
-        self.prefix_install = "%s/%s/_build" % (self.conanfile_directory, self.release_name)
         env_build = AutoToolsBuildEnvironment(self)
+        env_build.fpic = True
         with environment_append(env_build.vars):
-            udev = "--enable-udev" if self.options.udev else "--disable-udev"
-            prefix = "--prefix=%s" % self.prefix_install
-            self.run("cd %s && ./configure %s %s" % (self.release_name, prefix, udev))
-            self.run("cd %s && make" % self.release_name)
-            self.run("cd %s && make install" % self.release_name)
+            with chdir(self.release_name):                
+                shared_option = "--disable-static" if self.options.shared else "--disable-shared"
+                udev_option = "--enable-udev" if self.options.udev else "--disable-udev"
+                self.run("./configure --prefix=%s %s %s" % (self.build_dir, shared_option, udev_option))
+                self.run("make")
+                self.run("make install")
 
     def package(self):
-        # Copying headers
-        self.copy(
-            pattern="*.h",
-            dst="include",
-            src="%s/include" % self.prefix_install,
-            keep_path=True)
-        self.copy(
-            pattern="*.la",
-            dst="lib",
-            src="%s/lib" % self.prefix_install,
-            keep_path=False)
-        self.copy(
-            pattern="*.so*",
-            dst="lib",
-            src="%s/lib" % self.prefix_install,
-            keep_path=False)
-        self.copy(
-            pattern="*.pc",
-            dst="lib",
-            src="%s/lib" % self.prefix_install,
-            keep_path=True)
+        self.copy(pattern="*.h", dst="include", src=join(self.build_dir, "include"))
+        self.copy(pattern="*.a", dst="lib", src=join(self.build_dir, "lib"))
+        self.copy(pattern="*.so*", dst="lib", src=join(self.build_dir, "lib"))
 
     def package_info(self):
         self.cpp_info.libs = ['usb-1.0']
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("pthread")
+            if self.options.udev:
+                self.cpp_info.libs.append("udev")
