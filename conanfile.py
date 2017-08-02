ba05bd89a5aaeaaf7463eac165eb2c823d862a3e
Uilian Ries <uilianries@gmail.com>
2017-08-01 23:40:23 -0300

    #9 Added Cygwin path on Appveyor
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -72,8 +72,8 @@ class LibUSBConan(ConanFile):
                     configure_args.append('--enable-shared' if self.options.shared else '--disable-shared')
                     configure_args.append('--enable-static' if not self.options.shared else '--disable-static')
                     self._run_cmd("./configure %s" % ' '.join(configure_args))
-                    self.make(args=["all"])
-                    self.make(args=["install"])
+                    self.env_build.make(args=["all"])
+                    self.env_build.make(args=["install"])
 
     def package(self):
         self.copy("FindLIBUSB.cmake", ".", ".")
